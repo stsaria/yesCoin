2eb9fb04bd1d49c7144954a8abd29ee7e11584e2
stsaria <solothunder.autoer@gmail.com>
2024-06-15 08:06:16 +0900

    add nodes file save central server

--- src/centralApp.py
+++ src/centralApp.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, jsonify
+from etc import *
 
 app = Flask(__name__)
 
-nodes = []
-
 @app.route('/register', methods=['POST'])
 def registerNode():
+    global nodes
     node = request.get_json()
     if node not in nodes:
         nodes.append(node)
+    saveData(nodesFile, nodes)
     return jsonify(nodes), 201
 
 @app.route('/nodes', methods=['GET'])
 def getNodes():
-    return jsonify(nodes), 200
+    return jsonify(loadData(nodesFile)), 200
